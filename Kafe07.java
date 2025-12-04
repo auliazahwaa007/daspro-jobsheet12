4e1341984bd1e57c2b6c4c72c731a7a247ae7a66
auliazahwaa007 <auliazahwaa007@gmail.com>
2025-12-05 05:34:40 +0700

    Modifikasi 2

--- Kafe07.java
+++ Kafe07.java
@@ -11,8 +11,21 @@ public class Kafe07 {
         System.out.println("===============================");
         System.out.println("Silahkan pilih menu yang anda inginkan.");
     }
-    public static void main(String[] args) {
-         System.out.println("======= MENU RESTO KAFE =======");
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+         if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode promo: DISKON50 → Diskon 50%");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo: DISKON30 → Diskon 30%");
+        } else {
+            System.out.println("Kode promo invalid");
+        }
+        
+        System.out.println("======= MENU RESTO KAFE =======");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
@@ -21,6 +34,8 @@ public class Kafe07 {
         System.out.println("6. Mie Goreng  - 18,000");
         System.out.println("===============================");
         System.out.println("Silahkan pilih menu yang anda inginkan.");
-        Menu();
+    }
+    public static void main(String[] args) {
+        Menu("Andi", true, "Diskon30");
     }
 }
